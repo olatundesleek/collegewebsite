5cedb1fe8a18cdcea45f5585de9af952049a39ca
Olatunde Sleek <olatundesleek@gmail.com>
2020-07-01 14:30:04 +0100

    start the page content

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 let hamburger = document.querySelector(".hamburger");
 let mobileMenu = document.querySelector(".mobile-menu-list");
-
+let socialLinks = document.querySelector(".social-links");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
   mobileMenu.classList.toggle("show");
+  socialLinks.classList.toggle("margin");
 });
